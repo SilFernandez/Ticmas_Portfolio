a9cf33067e32ed8bb95c92c392efba230525eb1a
SilFernandez <102128832+SilFernandez@users.noreply.github.com>
2023-09-25 13:43:15 -0300

    modificaciones borre socialcomponent

--- ProyectoCV/src/app/app.module.ts
+++ ProyectoCV/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogosilComponent } from './components/logosil/logosil.component';
-import { SocialComponent } from './components/social/social.component';
+
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
@@ -20,7 +20,6 @@ import { FooterComponent } from './components/footer/footer.component';
     AppComponent,
     HeaderComponent,
     LogosilComponent,
-    SocialComponent,
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
